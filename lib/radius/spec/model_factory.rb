af50c300b5936c2cf002a7a72e1593a76153d6f6
Aaron Kromer <cupakromer@users.noreply.github.com>
2018-03-12 21:36:09 -0400

    Implement model factory instance creation helpers
    This implements the common `build` and `create` helpers for factories.
    The key features here are:
    
      - "safe" transformation of registered default attributes
    
        This duplicates the values to avoid array and hash modifications
        leaking across instances. This only shallow duplicates the values as
        that's generally enough. There are some edge cases with
        arrays of arrays, hashes with mutable values, and generally mutable
        objects. However, there is no standard deep dup implementation (but
        Rails does have one so maybe we'll add it in the future).
    
        We do not duplicate values which are frozen. This is a minor
        optimization to help cut down on extra object creations. In general,
        when an object is frozen the expectation is it cannot be modified;
        again there are exceptions with nested objects but in general
        there's an expectation that the frozen object has already taken
        responsibility to freeze it's mutable state or protect the mutable
        state when it was frozen.
    
      - registered default attribute values which are procs get `call`'d
    
        Because there are lots of objects which can respond to `call`, many
        of which are not generators, we need to be careful what we call
        `call` on. When we wrote the README we specified we were only going
        to support `Proc`. Other generators can be use by either converting
        them to procs (with `to_proc`) explicitly or wrapping them in a proc
        (e.g. `-> { some_generator.call }`).
    
        Often generators are slow and some are expensive (such as hitting
        the database - i.e. someone included another factory in it). Due to
        this we don't want to just blindly run every generator every time.
        Instead we only run the generators for those registered attributes
        which are not replaced by custom attributes provided to the factory
        helper.
    
      - remove any optional registered attributes which were not replaced by
        custom attributes provided to the factory helper
    
    A common Ruby idiom, and one that `ActiveRecord` implements, is to yield
    the instantiated object on the call to `new`. Sometimes this is simply
    called at the end of `initialize` other times it can be called before a
    finalization step (such as freezing) to allow final modifications to an
    object. We support this idiom by passing any block provided to the
    factory helpers through to `new`. Since any method can talk a block in
    Ruby, even if it doesn't use it, this is a benign action to take even
    for classes which don't support this idiom.
    
    At this time if an underlying class doesn't use the block it is just
    dropped on the floor. It would be nice to have the factory helpers
    "standardize" the idiom by ensuring the block is always called. However,
    this may not be straight forward and is not a feature we have supported
    in the past. We've left a TODO note in the code to reconsider this again
    in the future.
    
    The `create` implementation is essentially just `build` with a
    persistence call. This supports the standard Rails persistence method
    `save!`. The "bang method" is used over the regular `save` so that
    things fail fast and loudly; the `save!` version raises an exception on
    model validation failure. At this point there isn't another persistence
    API we need to support, but it wouldn't be too difficult to extend this
    to support another.

--- lib/radius/spec/model_factory.rb
+++ lib/radius/spec/model_factory.rb
@@ -4,6 +4,9 @@ module Radius
   module Spec
     # FIXME
     module ModelFactory
+      # FIXME
+      class TemplateNotFound < KeyError; end
+
       class << self
         # FIXME
         def catalog
@@ -17,9 +20,47 @@ module Radius
         alias_method :factory, :define_factory
 
         # @private
+        def safe_transform(value)
+          return value.call if value.is_a?(Proc)
+          return value if value.frozen?
+          value.dup
+        end
+
+        # @private
+        def template(name)
+          templates.fetch(name) {
+            raise TemplateNotFound, "template not found: #{name}"
+          }
+        end
+
+        # @private
         def templates
           @templates ||= {}
         end
+      end
+
+    module_function
+
+      # FIXME
+      def build(name, custom_attrs = {}, &block)
+        name = name.to_s
+        template = ::Radius::Spec::ModelFactory.template(name)
+        template_only = template.keys - custom_attrs.keys
+        attrs = template.slice(*template_only)
+                        .delete_if { |_, v| :optional == v }
+                        .transform_values! { |v|
+                          ::Radius::Spec::ModelFactory.safe_transform(v)
+                        }
+                        .merge(custom_attrs)
+        # TODO: Always yield to the provided block even if new doesn't
+        ::Object.const_get(name).new(attrs, &block)
+      end
+
+      # FIXME
+      def create(name, custom_attrs = {}, &block)
+        instance = build(name, custom_attrs, &block)
+        instance.save! if instance.respond_to?(:save!)
+        instance
       end
     end
   end
--- spec/radius/spec/model_factory_spec.rb
+++ spec/radius/spec/model_factory_spec.rb
@@ -155,4 +155,322 @@ RSpec.describe Radius::Spec::ModelFactory do
       }.to yield_with_args(Radius::Spec::ModelFactory)
     end
   end
+
+  specify "building an instance which is not registered " \
+          "raises `TemplateNotFound`" do
+    expect {
+      Radius::Spec::ModelFactory.build "AnyClass"
+    }.to raise_error(
+      Radius::Spec::ModelFactory::TemplateNotFound,
+      "template not found: AnyClass",
+    )
+  end
+
+  specify "building an instance for a non-existant class raises `NameError`" do
+    Radius::Spec::ModelFactory.define_factory "AnyClass"
+    expect {
+      Radius::Spec::ModelFactory.build "AnyClass"
+    }.to raise_error NameError, "uninitialized constant AnyClass"
+  end
+
+  describe "building an instance" do
+    it "does not require custom attributes" do
+      stub_const "AnyClass", Struct.new(:arg1, :arg2, keyword_init: true)
+      Radius::Spec::ModelFactory.define_factory(
+        "AnyClass",
+        arg1: "Any Default Arg1 Value",
+        arg2: "Any Default Arg2 Value",
+      )
+
+      an_instance = Radius::Spec::ModelFactory.build("AnyClass")
+      expect(an_instance).to be_an_instance_of(AnyClass).and have_attributes(
+        arg1: "Any Default Arg1 Value",
+        arg2: "Any Default Arg2 Value",
+      )
+    end
+
+    it "merges the custom attributes with the registered ones" do
+      stub_const "AnyClass", Struct.new(:arg1, :arg2, keyword_init: true)
+      Radius::Spec::ModelFactory.define_factory(
+        "AnyClass",
+        arg1: "Any Default Arg1 Value",
+        arg2: "Any Default Arg2 Value",
+      )
+
+      an_instance = Radius::Spec::ModelFactory.build(
+        "AnyClass",
+        arg1: "Custom Value",
+      )
+      expect(an_instance).to be_an_instance_of(AnyClass).and have_attributes(
+        arg1: "Custom Value",
+        arg2: "Any Default Arg2 Value",
+      )
+
+      an_instance = Radius::Spec::ModelFactory.build(
+        "AnyClass",
+        arg2: "Custom Value",
+      )
+      expect(an_instance).to be_an_instance_of(AnyClass).and have_attributes(
+        arg1: "Any Default Arg1 Value",
+        arg2: "Custom Value",
+      )
+    end
+
+    it "includes custom attributes which were not registered" do
+      stub_const "AnyClass", Struct.new(:arg1, :arg2, keyword_init: true)
+      Radius::Spec::ModelFactory.define_factory(
+        "AnyClass",
+        arg1: "Any Default Arg1 Value",
+      )
+      an_instance = Radius::Spec::ModelFactory.build(
+        "AnyClass",
+        arg2: "Custom Value",
+      )
+
+      expect(an_instance).to be_an_instance_of(AnyClass).and have_attributes(
+        arg1: "Any Default Arg1 Value",
+        arg2: "Custom Value",
+      )
+    end
+
+    it "duplicates registered attributes to prevent state leak " \
+       "between instances", :aggregate_failures do
+      mutable_array = %i[any value]
+      stub_const "AnyClass", Struct.new(:arr, keyword_init: true)
+      Radius::Spec::ModelFactory.define_factory "AnyClass", arr: mutable_array
+      an_instance = Radius::Spec::ModelFactory.build("AnyClass")
+      another_instance = Radius::Spec::ModelFactory.build("AnyClass")
+
+      aggregate_failures "produces expected values" do
+        expect(an_instance.arr).to eq mutable_array
+        expect(another_instance.arr).to eq mutable_array
+      end
+
+      aggregate_failures "values are duplicates" do
+        expect(an_instance.arr).not_to be mutable_array
+        expect(another_instance.arr).not_to be mutable_array
+      end
+
+      expect {
+        an_instance.arr << :modification
+      }.not_to change {
+        another_instance.arr
+      }.from(mutable_array)
+    end
+
+    it "does not duplicate frozen registered attributes" do
+      frozen_array = %i[any value].freeze
+      stub_const "AnyClass", Struct.new(:arr, keyword_init: true)
+      Radius::Spec::ModelFactory.define_factory "AnyClass", arr: frozen_array
+      an_instance = Radius::Spec::ModelFactory.build("AnyClass")
+
+      expect(an_instance.arr).to be frozen_array
+    end
+
+    it "does not duplicate custom attributes", :aggregate_failures do
+      stub_const "AnyClass", Struct.new(:arr, keyword_init: true)
+      Radius::Spec::ModelFactory.define_factory "AnyClass", arr: nil
+      shared_array = %i[any value]
+      an_instance = Radius::Spec::ModelFactory.build(
+        "AnyClass",
+        arr: shared_array,
+      )
+      another_instance = Radius::Spec::ModelFactory.build(
+        "AnyClass",
+        arr: shared_array,
+      )
+
+      aggregate_failures "values are not duplicates" do
+        expect(an_instance.arr).to be shared_array
+        expect(another_instance.arr).to be shared_array
+      end
+
+      expect {
+        shared_array << :modification
+      }.to change {
+        an_instance.arr
+      }.to(
+        %i[any value modification]
+      ).and change {
+        another_instance.arr
+      }.to(
+        %i[any value modification]
+      )
+    end
+
+    it "transforms registered attribute generators", :aggregate_failures do
+      counter = 0
+      stub_const "AnyClass", Struct.new(:arg, keyword_init: true)
+      Radius::Spec::ModelFactory.define_factory "AnyClass",
+                                                arg: -> { counter += 1 }
+      expect {
+        Radius::Spec::ModelFactory.build "AnyClass"
+      }.to change {
+        counter
+      }.from(0).to(1)
+
+      expect {
+        expect(Radius::Spec::ModelFactory.build("AnyClass").arg).to eq 2
+      }.to change {
+        counter
+      }.from(1).to(2)
+    end
+
+    it "does not transform other objects which respond to call" do
+      non_proc_callable = double("Callable")
+      allow(non_proc_callable).to receive(:dup).and_return(non_proc_callable)
+      allow(non_proc_callable).to receive(:call).and_raise(
+        ":call was called on a non-Proc object"
+      )
+      stub_const "AnyClass", Struct.new(:arg, keyword_init: true)
+      Radius::Spec::ModelFactory.define_factory "AnyClass",
+                                                arg: non_proc_callable
+      an_instance = Radius::Spec::ModelFactory.build("AnyClass")
+
+      expect(an_instance.arg).to be non_proc_callable
+    end
+
+    it "does not transform custom attribute procs" do
+      non_proc_callable = double("Callable")
+      allow(non_proc_callable).to receive(:call).and_raise(
+        ":call was called on a non-Proc object"
+      )
+      stub_const "AnyClass", Struct.new(:arg, keyword_init: true)
+      Radius::Spec::ModelFactory.define_factory "AnyClass",
+                                                arg: non_proc_callable
+      an_instance = Radius::Spec::ModelFactory.build(
+        "AnyClass",
+        arg: non_proc_callable,
+      )
+
+      expect(an_instance.arg).to be non_proc_callable
+    end
+
+    it "does not transform replaced registered attribute generators" do
+      counter = 0
+      stub_const "AnyClass", Struct.new(:arg, keyword_init: true)
+      Radius::Spec::ModelFactory.define_factory "AnyClass",
+                                                arg: -> { counter += 1 }
+      expect {
+        Radius::Spec::ModelFactory.build "AnyClass", arg: :anything
+      }.not_to change {
+        counter
+      }.from(0)
+    end
+
+    it "removes optional registered attributes" do
+      stub_const "AnyClass", Struct.new(:arg, keyword_init: true)
+      Radius::Spec::ModelFactory.define_factory "AnyClass",
+                                                arg: :optional
+
+      an_instance = Radius::Spec::ModelFactory.build("AnyClass")
+      expect(an_instance.arg).to be nil
+
+      an_instance = Radius::Spec::ModelFactory.build("AnyClass", arg: :custom)
+      expect(an_instance.arg).to eq :custom
+    end
+
+    it "pass any provided block to the object's initializer" do
+      block_initialized = false
+      stub_const(
+        "AnyClass",
+        Class.new {
+          def initialize(*)
+            yield
+          end
+        },
+      )
+      Radius::Spec::ModelFactory.define_factory "AnyClass"
+
+      expect {
+        Radius::Spec::ModelFactory.build("AnyClass") do
+          block_initialized = true
+        end
+      }.to change {
+        block_initialized
+      }.to true
+    end
+  end
+
+  describe "creating an instance" do
+    it "builds the instance based on the template and custom attributes",
+       :aggregate_failures do
+      counter = 0
+      block_initialized = false
+      stub_const(
+        "AnyClass",
+        Struct.new(:arg1, :arg2, :arg3, keyword_init: true) {
+          def initialize(*)
+            super
+            yield self
+          end
+        },
+      )
+      Radius::Spec::ModelFactory.define_factory "AnyClass",
+                                                arg1: :any_value,
+                                                arg2: -> { counter += 1 }
+      custom_attrs = {
+        arg1: :custom,
+        arg3: :additional_value,
+      }
+
+      # rubocop:disable Metrics/LineLength
+      an_instance = nil
+      expect {
+        an_instance = Radius::Spec::ModelFactory.create("AnyClass", custom_attrs) { |obj|
+          block_initialized = obj
+        }
+      }.to change {
+        block_initialized
+      }.from(false)
+      # rubocop:enable Metrics/LineLength
+      expect(block_initialized).to be an_instance
+      expect(an_instance).to be_an_instance_of(AnyClass).and have_attributes(
+        arg1: :custom,
+        arg2: 1,
+        arg3: :additional_value,
+      )
+    end
+
+    it "persists objects which respond to `save!`" do
+      stub_const(
+        "AnyClass",
+        Struct.new(:save_called, keyword_init: true) {
+          def save!
+            self.save_called = true
+          end
+        },
+      )
+      Radius::Spec::ModelFactory.define_factory "AnyClass"
+
+      an_instance = Radius::Spec::ModelFactory.create(
+        "AnyClass",
+        save_called: false,
+      )
+      expect(an_instance.save_called).to be true
+    end
+
+    it "yields to the provided block before calling `save!`" do
+      call_order = []
+      stub_const(
+        "AnyClass",
+        Struct.new(:arg, keyword_init: true) { |klass|
+          klass.define_method(:initialize) do |*|
+            call_order << :initialize
+          end
+
+          klass.define_method(:save!) do
+            call_order << :save!
+          end
+        },
+      )
+      Radius::Spec::ModelFactory.define_factory "AnyClass"
+
+      expect {
+        Radius::Spec::ModelFactory.create("AnyClass")
+      }.to change {
+        call_order
+      }.from([]).to(%i[initialize save!])
+    end
+  end
 end
